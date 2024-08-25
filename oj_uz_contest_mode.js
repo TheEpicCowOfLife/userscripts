f386e7c1137730e8d2a78b00575c9fbf97873189
Quang Ong <38208392+TheEpicCowOfLife@users.noreply.github.com>
2024-08-25 21:17:42 +1000

    Create oj_uz_contest_mode.js

---
+++ oj_uz_contest_mode.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         oj.uz contest mode
+// @namespace    http://tampermonkey.net/
+// @version      2024-08-25
+// @description  Hides information on oj.uz about other user submissions to better simulate a contest environment
+// @author       You
+// @match        https://oj.uz/problem/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// @copyright 2012+, hibbard.eu
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const hideElements = () => {
+        $('a:contains("Statistics")').hide();
+        $('a:contains("Submissions")').hide();
+        $('a:contains("Submissions")').hide();
+        const tableRow = $('th:contains("# of submissions")').closest('table').find('tbody tr');
+        tableRow.find("td:nth-child(3)").hide()
+        tableRow.find("td:nth-child(4)").hide()
+        tableRow.find("td:nth-child(5)").hide()
+    }
+    hideElements()
+    // Your code here...
+    $(document).ready(hideElements);
+})();
